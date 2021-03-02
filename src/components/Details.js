e611547180f8443610dc919021f0190072a2dcdb
kamilgw <kamilgw1@gmail.com>
2021-03-02 19:41:27 +0100

    feat: change pokemon details layout

---
+++ src/components/Details.js
@@ -0,0 +1,160 @@
+import React, {Fragment, useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  ActivityIndicator,
+  ScrollView,
+} from 'react-native';
+import {colors} from '../styles';
+import {ProgressBar} from 'react-native-paper';
+const Details = props => {
+  const [details, setDetails] = useState ([]);
+
+  useEffect (() => {
+    fetchPokemonDetails ();
+  }, []);
+
+  const fetchPokemonDetails = () => {
+    const {state} = props.navigation;
+    fetch (`https://pokeapi.co/api/v2/pokemon/${state.params.pokemon}`)
+      .then (res => res.json ())
+      .then (details => setDetails (details));
+  };
+
+  return details.name
+    ? <ScrollView contentContainerStyle={styles.container}>
+        <View
+          style={{
+            ...styles.header,
+            backgroundColor: colors[details.types[0].type.name],
+          }}
+        >
+          <View style={styles.info}>
+            <Text style={styles.name}>{details.name}</Text>
+            <View style={styles.teste}>
+              <Image
+                style={styles.image}
+                source={{
+                  uri: `https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/${String (details.id).padStart (3, '0')}.png`,
+                }}
+              />
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.body}>
+          <Fragment>
+            <Text style={styles.title}>About</Text>
+            <Text style={styles.description}>
+              Something here 
+            </Text>
+
+              <View style={styles.viewHeightWeight}>
+                <View style={styles.dataHeightWeight}>
+                  <Text style={styles.titleHeightWeight}>Height</Text>
+                  <Text>{details.height}</Text>
+                </View>
+                <View style={styles.dataHeightWeight}>
+                  <Text style={styles.titleHeightWeight}>Weight</Text>
+                  <Text>{details.weight}</Text>
+                </View>
+              </View>
+
+            <View style={{}}>
+              {details.stats.map ((item, index) => {
+                return (
+                  <View key={index}>
+                    <Text>{item.stat.name}</Text>
+                    <ProgressBar
+                      style={{height: 20}}
+                      progress={parseInt (item.base_stat) / 100}
+                      color={colors[details.types[0].type.name]}
+                    />
+                  </View>
+                );
+              })}
+            </View>
+          </Fragment>
+        </View>
+      </ScrollView>
+    : <View style={styles.indicator}>
+        <ActivityIndicator size="large" color="#E63F34" />
+      </View>;
+};
+
+export default Details;
+
+const styles = StyleSheet.create ({
+  header: {
+    height: 250,
+  },
+  body: {
+    marginLeft: 15,
+    marginRight: 15,
+  },
+  title: {
+    fontSize: 20,
+    color: '#303943',
+    lineHeight: 42,
+    fontWeight: 'bold',
+  },
+  container: {
+    // height: "100%"
+  },
+  name: {
+    fontSize: 30,
+    color: '#303943',
+    lineHeight: 42,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  info: {
+    marginLeft: 15,
+    height: '100%',
+  },
+  image: {
+    height: 200,
+    width: 200,
+  },
+  types: {
+    flexDirection: 'row',
+    marginTop: 5,
+  },
+  type: {
+    backgroundColor: 'rgba(255,255,255,0.2)',
+    fontSize: 11,
+    color: 'white',
+    padding: 3,
+    borderRadius: 15,
+    marginRight: 5,
+    textAlign: 'center',
+    width: 50,
+  },
+  teste: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  description: {
+    marginRight: 15,
+  },
+  card: {
+    flexDirection: 'row',
+  },
+  viewHeightWeight: {
+    flexDirection: 'row',
+  },
+  dataHeightWeight: {
+    flex: 1,
+    alignItems: 'center',
+    marginBottom: 12,
+    marginTop: 12,
+    fontSize: 14,
+  },
+  titleHeightWeight: {
+    color: '#acb0b4',
+    marginBottom: 5,
+  },
+});
--- src/styles/index.js
+++ src/styles/index.js
@@ -1,24 +1,103 @@
 
 export const colors = {
-  Plant: '#72B357',
-  Fire: '#DC5E45',
-  Water: '#599FEC',
-  Psychic: '#D2436D',
-  Dragon: '#333F96',
-  Ice: '#77E3FB',
-  Grass: '#74b264',
-  Poison: '#8b31b1',
-  Bug: '#a1c661',
-  Rock: '#b5a04b',
-  Electric: '#F3CA45',
-  Normal: '#a9a87e',
-  Ground: '#c8c075',
-  Dark: '#6d594a',
-  Flying: '#a294ea',
-  Fighting: '#b33c31',
-  Ghost: '#6c5b94',
+  plant: '#72B357',
+  fire: '#DC5E45',
+  water: '#599FEC',
+  psychic: '#D2436D',
+  dragon: '#333F96',
+  ice: '#77E3FB',
+  grass: '#74b264',
+  poison: '#8b31b1',
+  bug: '#a1c661',
+  rock: '#b5a04b',
+  electric: '#F3CA45',
+  normal: '#a9a87e',
+  ground: '#c8c075',
+  dark: '#6d594a',
+  flying: '#a294ea',
+  fighting: '#b33c31',
+  ghost: '#6c5b94',
   default: {
     foregroundText: '#A4A7A9',
     activeText: '#555',
   },
 };
+
+// const PokemonUtil = {
+//         getColor: (types) => {
+//         let substract = 1;
+
+//         if(types === undefined){
+//             return "#4FC1A6";
+//         }
+
+//         if(types.length > 1 && types[types.length - substract].type.name === 'normal'){
+//             substract++;
+//         }
+
+//         switch (types[types.length - substract].type.name) {
+//             case 'fire':
+//                 return "#F7786B"
+//                 break;
+
+//             case 'water':
+//                 return "#77C4FE"
+//                 break; 
+                
+//             case 'poison':
+//                 return "#7C538C"
+//                 break;     
+        
+//             case 'grass':
+//                 return "#4FC1A6";
+//                 break;
+
+//             case 'electric':
+//                 return "#FFCE4B";
+//                 break;   
+            
+//             case 'rock':
+//                 return "#B1736C";
+//                 break;   
+            
+//             case 'dark':
+//                 return "#565669";
+//                 break;
+            
+//             case 'flying':
+//                 return "#cdcde6";
+//                 break;
+            
+//             case 'dragon':
+//                 return "#f7af5a";
+//                 break;
+
+//             case 'bug':
+//                 return "#92df68";
+//                 break; 
+
+//             case 'ground':
+//                 return "#be7447";
+//                 break;
+
+//             case 'psychic':
+//                 return "#405483";
+//                 break;
+
+//             case 'fighting':
+//                 return "#a2a29b";
+//                 break;
+//             case 'ghost':
+//                 return '#9473b4';
+//                 break;
+//             case 'ice':
+//                 return '#a4def6';
+//                 break;
+
+//             default:
+//                 return "#c5c5c5"
+//         }
+//     }
+// }
+
+// export default PokemonUtil;
